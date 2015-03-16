2ba0037238decc917361cbe064db8998b8ad8e58
Worathiti Manosroi <w.manosroi@gmail.com>
2015-03-16 11:54:32 +0100

    start out ...

---
+++ jrpg.js
@@ -0,0 +1,12 @@
+var jrpg = {
+    vars: {
+        textArray: [],
+        var2: 'value2'
+    },
+    func1: function () {
+        return this.vars.var1;
+    },
+    func2: function () {
+        alert("This is func2");
+    }
+};
